213aa94d6686c6596a1b2e38b82d0939440a0843
krishna <krishna.prasath.subramanian@rwth-aachen.de>
2014-01-11 20:35:29 +0100

    testing if the function is the one causing the problem

---
+++ R/loadFile.R
@@ -0,0 +1,13 @@
+loadFile <- function(filePath)
+{
+    fileType = substr(filePath, nchar(filePath) - 3 + 1, nchar(filePath));
+    
+    if(fileType == "txt")
+        dataset <- read.table(filePath, head=T);
+    if(fileType == "csv")
+        dataset <- read.csv(filePath, head=T);
+    
+    variableNames = names(dataset);
+    
+    list(dataset = dataset, variableNames = variableNames);    
+}
